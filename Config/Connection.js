bc13c14722e0e268b0cd4987b6d0408e15d6ea34
Deepak ts <tsdeepak589@gmail.com>
2024-04-19 00:00:33 +0530

    env added

--- Config/Connection.js
+++ Config/Connection.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/studycap').then(()=>{
-    console.log("DB connected");
-// mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(()=>{
+// mongoose.connect('mongodb://127.0.0.1:27017/studycap').then(()=>{
 //     console.log("DB connected");
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(()=>{
+    console.log("DB connected");
 })
 .catch(()=>{
     console.log("DB not connected");
